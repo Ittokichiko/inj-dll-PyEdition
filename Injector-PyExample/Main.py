4bcd2ebba5bbc0e1fe53ce32de1d78f7701ca1b6
CheeZe Dark(Rikko Matsumato) <110032482+CheeZeDark@users.noreply.github.com>
2025-08-20 21:34:08 +0300

    Add files via upload

---
+++ Injector-PyExample/Main.py
@@ -0,0 +1,17 @@
+import ctypes as ct
+import os
+from os import _exit as ex_function
+import psutil
+def main():
+    input_dllname = input("Please Write You're DLL Name(you need also with full name of folder for working my script!!!): ")
+    procname = input("You're Process Name: ")
+    injector = ct.WinDLL("injector.dll")
+    for proc in psutil.process_iter(['pid', 'name']):
+        if proc.info['name'] == procname:
+            pid = proc.info['pid']
+            injector.Inject(pid, bytes("{}".format(input_dllname), "utf-8"))
+        else:
+            raise Exception("Not Founded {}".format(proc.info['name']))
+
+if __name__ == "__main__":
+    main()    
